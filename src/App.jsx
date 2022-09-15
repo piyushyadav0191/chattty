b1b4239ed2b8a3d86e93cb910c032cbd243f884e
piyush yadav <84402719+piyushyadav0191@users.noreply.github.com>
2022-09-15 11:24:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Chat from './pages/chat';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import io from 'socket.io-client';
 
-const socket = io.connect('http://localhost:4000');
+const socket = io.connect('https://chatty-server-eight.vercel.app/');
 
 function App() {
   const [username, setUsername] = useState('');
